513321ef226ed9d1b1a9f9883a83e6d7343c25db
Kuo0418 <85402557+dmd10705@users.noreply.github.com>
2022-02-23 09:29:45 +0800

    0223

---
+++ EmotionFactory/Assets/Script/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+   public void Fretful()
+    {
+        SceneManager.LoadScene("煩躁");
+    }
+
+    public void Melancholy()
+    {
+        SceneManager.LoadScene("憂鬱");
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("初始");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ EmotionFactory/Assets/Script/ScenesManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager : MonoBehaviour
+{
+    private void Start()
+    {
+        print("憂鬱");
+
+        
+        if (true)
+        {
+            SceneManager.LoadScene("憂鬱");
+        }
+    }
+}
